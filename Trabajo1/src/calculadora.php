0b6757ac31a877d5f437f48a3766319b3844cd49
cvaclop1911 <126626460+cvaclop1911@users.noreply.github.com>
2025-01-14 18:22:08 +0100

    Actualizacion de calculadora, mostrar mensaje y otro try catch para introduccion de numeros con letras

--- Trabajo1/src/calculadora.php
+++ Trabajo1/src/calculadora.php
@@ -1,55 +1,67 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $num1 = $_POST["num1"];
-    $num2 = $_POST["num2"];
-    $res = 0;
-    if (is_numeric($num1) || is_numeric($num2)) {
-        if ($_POST["op"] == "+") {
-            $res = (float)$num1 + (float)$num2;
-        }
-        if ($_POST["op"] == "-") {
-            $res = (float)$num1 - (float)$num2;
-        }
-        if ($_POST["op"] == "*") {
-            $res = (float)$num1 * (float)$num2;
-        }
-        if ($_POST["op"] == "/") {
-            try {
-                $res = (float)$num1 / (float)$num2;
-            } catch (Error $e) {
-                $res = "No se puede dividir entre 0";
-            }
-        }
-    } else {
-        $res = "Solo admite numeros";
-    }
-}
-?>
 <!DOCTYPE html>
 <html lang="es">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
 
-<body>
-    <h2>Calculadora</h2>
-    <form action="./calculadora.php" method="post">
-        <input type="text" name="num1" id="num1">
-        <select name="op" id="op">
-            <option value="+">+</option>
-            <option value="-">-</option>
-            <option value="*">*</option>
-            <option value="/">/</option>
-        </select>
-        <input type="text" name="num2" id="num2">
-        =
-        <input type="text" value="<?php echo $res; ?>" size="30"> <br> <br>
-        <input type="submit" value="Calcular">
-        <input type="reset" value="Borrar">
-    </form>
-</body>
+    <body>
+        <h2>Calculadora</h2>
+        <form action="./prueba.php" method="post">
+            <input type="text" name="num1" id="num1">
+            <select name="op" id="op">
+                <option value="+">+</option>
+                <option value="-">-</option>
+                <option value="*">*</option>
+                <option value="/">/</option>
+            </select>
+            <input type="text" name="num2" id="num2">
+            <input type="submit" value="Calcular">
+        </form>
 
+        <?php
+        $num1;
+        $num2;
+        $res = 0.0;
+        $error = "";
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $num1 = $_POST["num1"];
+            $num2 = $_POST["num2"];
+            try {
+                if ($_POST["op"] == "+") {
+                    $res = $num1 + $num2;
+                }
+                if ($_POST["op"] == "-") {
+                    $res = $num1 - $num2;
+                }
+                if ($_POST["op"] == "*") {
+                    $res = $num1 * $num2;
+                }
+                if ($_POST["op"] == "/") {
+                    try {
+                        $res = $num1 / $num2;
+                    } catch (Error $e) {
+                        $error = "No se puede dividir entre 0";
+                    }
+                }
+            } catch (Error $e) {
+                $error = "Solo admite numeros";
+            }
+        }
+        ?>
+        <p>
+            <!-- Mostrar resultado o error -->
+            <?php
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                if (!$error) {
+                    echo $num1 . " " . $_POST["op"] . " " . $num2 . " = " . $res;
+                } else {
+                    echo $error;
+                }
+            }
+            ?>
+        </p>
+    </body>
 </html>
