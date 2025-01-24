9781c6ca7963bb10f14524855332de06f862400c
BlackCherryCat <blackcherry@danwin1210.de>
2025-01-24 20:32:45 +0100

    Actualizado Trait BD

--- Trabajo3/src/BD.php
+++ Trabajo3/src/BD.php
@@ -1,47 +1,54 @@
 <?php
 trait BD
 {
-    public function guardar($usuario, $contraseña, $dato)
+    public function guardar($usuario, $contraseña, $id, $nombre, $precio, $stock, $descripcion)
     {
         try {
             $conexion = mysqli_connect("localhost", $usuario, $contraseña, "Trabajo3");
-            $query = "INSERT INTO Productos (nombreProducto,precio,stock) VALUES ('$dato[0]',$dato[1],$dato[2])";
+            $query = "INSERT INTO producto (idProducto, nombre, precio, stock, descripcion) VALUES ('$usuario', '$contraseña', '$id', '$nombre', '$precio', '$stock', '$descripcion')";
             mysqli_query($conexion, $query);
         } catch (Exception $e) {
             echo "Error al guardar";
         }
     }
-    public function eliminar($usuario, $contraseña, $dato)
+    public function eliminar($usuario, $contraseña, $id)
     {
         try {
             $conexion = mysqli_connect("localhost", $usuario, $contraseña, "Trabajo3");
-            $query = "DELETE FROM Productos WHERE nombreProducto='$dato'";
+            $query = "DELETE FROM producto WHERE idProducto='$id'";
             mysqli_query($conexion, $query);
         } catch (Exception $e) {
             echo "Error al eliminar";
         }
     }
-    public function modificar($usuario, $contraseña, $dato1, $dato2)
+    public function modificar($usuario, $contraseña, $campo, $datoactual, $datonuevo)
     {
         try {
             $conexion = mysqli_connect("localhost", $usuario, $contraseña, "Trabajo3");
-            $query = "UPDATE Productos SET nombreProducto='$dato2' WHERE nombreProducto='$dato1'";
+            $query = "UPDATE producto SET '$campo'='$datonuevo' WHERE '$campo'='$datoactual'";
             mysqli_query($conexion, $query);
         } catch (Exception $e) {
             echo "Error al modificar";
         }
     }
-    public function consultar($usuario, $contraseña, $dato)
+    public function consultar($usuario, $contraseña, $id)
     {
         try {
             $conexion = mysqli_connect("localhost", $usuario, $contraseña, "Trabajo3");
-            $query = "SELECT * FROM Productos WHERE nombreProducto='$dato'";
+            $query = "SELECT * FROM producto WHERE idProducto='$id'";
             $resultado = mysqli_query($conexion, $query);
+            echo "<table border=1>";
+            echo "<tr>";
+            echo "<th>ID de Producto</th>";
+            echo "<th>Nombre</th>";
+            echo "<th>Precio</th>";
+            echo "<th>Stock</th>";
+            echo "<th>Descripcion</th>";
+            echo "</tr>";
             while ($registro = mysqli_fetch_assoc($resultado)) {
-                echo "Id |" . $registro["IdProducto"];
-                echo "| Nombre " . "|" . $registro["nombreProducto"] . "|";
-                echo "<br/>";
+                echo "<tr>" . "<td>" . $registro["idProducto"] . "</td>" .  "<td>" . $registro["nombre"] . "</td>" .  "<td>" . $registro["precio"] . "</td>" . "<td>" . $registro["stock"] . "</td>" . "<td>" . $registro["descripcion"] . "</td>" . "</tr>";
             }
+            echo "</table>";
         } catch (Exception $e) {
             echo "Error al consultar";
         }
