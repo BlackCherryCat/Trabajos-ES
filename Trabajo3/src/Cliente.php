5d629229a7db57cd6d484ab421891a0f26f6a279
Jorge170805 <jorgesegoviasanchez170805@gmail.com>
2025-01-24 20:50:52 +0100

    Clase Cliente creada

---
+++ Trabajo3/src/Cliente.php
@@ -0,0 +1,55 @@
+<?php
+    enum MetodoDePago: string {
+        case TARJETA = 'tarjeta';
+        case PAYPAL = 'paypal';
+        case CHEQUE = 'cheque';
+    }
+    
+    class Cliente {
+        private $compras;
+        private $carrito;
+        private MetodoDePago $metodoPago;
+
+        public function _construct(MetodoDePago $metodoDePago) {
+            $this->compras = [];
+            $this->carrito = [];
+            $this->metodoDePago = $metodoDePago;
+        }
+
+        public function getCompras() {
+            return $this->compras;
+        }
+
+        public function getCarrito() {
+            return $this->carrito;
+        }
+
+        public function getMetodoPago() {
+            return $this->metodoDePago;
+        }
+
+        public function agregarCarrito(Producto $producto) {
+            $this->carrito[] = $producto;
+        }
+
+        public function cambiarMetodoDePago(MetodoDePago $nuevoMetodo) {
+            $this->metodoDePago = $nuevoMetodo;
+        }
+
+        public function realizarPedido() {
+            if (!empty($this->carrito)) {
+                $this->compras[] = $this->carrito;
+                $this->vaciarCarrito();
+                echo "Pedido realizado con éxito.";
+            } else {
+                echo "El carrito está vacío, no se puede realizar el pedido.";
+            }
+        }
+
+        public function vaciarCarrito() {
+                $this->carrito = [];
+        }
+    }
+
+
+?>
