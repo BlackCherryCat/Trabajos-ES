a750c96a5745e2beb38494cb96bf3a5183f8bcda
cvaclop1911 <126626460+cvaclop1911@users.noreply.github.com>
2025-01-24 20:42:42 +0100

    Creacion de registro y actualizacion de login

--- Trabajo3/src/signup.php
+++ Trabajo3/src/signup.php
@@ -3,26 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Iniciar sesión</title>
+    <title>Registrarse</title>
 </head>
 <body>
-    <h1>Iniciar Sesión</h1>
-    <form action="login.php" method="POST">
+    <h1>Registrarse</h1>
+    <form action="signup.php" method="POST">
         <label for="user">Usuario: </label><input type="text" name="user" require /><br><br>
         <label for="pass">Contraseña: </label><input type="password" name="pass" require /><br><br>
+        <select name="tipoUsuario" id="tipoUsuario" required>
+            <option value="empleado">Empleado</option>
+            <option value="administrador">Administrador</option>
+        </select>
         <input type="submit" value="Enviar" />
 
-        <p> Si no tienes un usuario <a href="create.php">haz click aquí</a></p>
+        <p> Si ya tienes un usuario <a href="login.php">haz click aquí</a></p>
     </form>
 </body>
 </html>
-
-<?php
-
-    session_start();
-
-    if($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["user"]) && isset($_POST["pass"])){
-
-    }
-    
-?>
