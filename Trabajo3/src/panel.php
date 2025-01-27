93ae77b40a8e1a3628fdf469f25fab477f01d8f7
BlackCherryCat <blackcherry@danwin1210.de>
2025-01-27 21:30:54 +0100

    Update panel.php

---
+++ Trabajo3/src/panel.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    require_once "clases\AdminClass.php";
+    session_start();
+    $admin = $_SESSION['admmin'];
+    echo "<h1>Bienvenido " . $_SESSION['user'] . "</h1>";
+    ?>
+    <h1>Panel de control</h1>
+    <!-- <button onclick="<?php  ?>"></button> -->
+    <a href="carrito.php">Ver carrito</a>
+    <a href="compras.php">Ver compras</a>
+    <a href="logout.php">Cerrar sesión</a>
+</body>
+
+</html>
