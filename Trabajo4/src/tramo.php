dca4e267e41d214b356c2a4af07a615eafb34d1c
jaVillena17 <javillena13@gmail.com>
2025-02-17 19:30:40 +0100

    interfaz básica tramos

--- Trabajo4/src/tramo.php
+++ Trabajo4/src/tramo.php
@@ -1,5 +1,6 @@
 <?php
     require_once './includes/header.php';
+    include_once './includes/conexion.php';
 
     if(isset($_GET["date"])){
         //Tratamiento de la fecha
@@ -8,12 +9,39 @@
         $tmpFecha = explode("-", $fecha);
         $fecha = implode("/", $tmpFecha);
 
-        $today = date("d-m-Y", (time() +strtotime($fecha)));
+        $today = date("Y-m-d", (time() +strtotime($fecha)));
         $day = date("d", strtotime($fecha));
         $month = monthString(date("m", strtotime($fecha)));
         $year = date("o", strtotime($fecha));
 
-        echo $day." de ".$month." de ".$year;
+        echo "<h1>".$day." de ".$month." de ".$year."</h1>";
+        //Imprimir los tramos
+
+        echo "<table class='tramo'><tr><th>08:30-09:30</th><th>09:30-10:30</th><th>10:30-11:30</th><th>11:30-12:30</th><th>12:30-13:30</th><th>13:30-14:30</th></tr>";
+
+        $horario = ["08:30", "09:30", "10:30", "11:30", "12:30", "12:30", "14:30"];
+
+        $i = 0;
+
+        //Aqui, hay que hacer sql, sacar las plazas disponibles por cada tramo y imprimir el tramo completo, por colores. Queda controlar el horario y dia
+        $query = "select Reserva_Tramos.IdTramo, Reserva.Fecha, Tramo.Horario, sum(Reservas.NumAlumnos) As 'Alumnos'
+                    from Reservas
+                    right join Reserva_Tramos on Reservas.IdReserva = Reserva_Tramos.IdReserva
+                    inner join Tramos on Reserva_Tramos.IdTramo = Tramos.IdTramo
+                    where Reserva.Fecha = '.$today.' AND
+                    Tramo.Horario = '.$horario[$i].'
+                    group by Reserva_Tramos.IdTramo;";
+
+        //Hay que sacar el número de alumnos
+
+        echo "<tr><td></td><td></td><td></td><td></td><td></td><td></td></tr></table>";
+
+
+
+
+
+
+
     }else{
         header('location: reserva.php');
         exit();
